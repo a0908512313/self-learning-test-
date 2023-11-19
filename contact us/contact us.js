bec052451db29b1f22ddbcd2216383960a5e09f8
a0908512313 <a0908512313@gmail.com>
2023-11-19 12:33:46 +0800

    sent form

--- contact us/contact us.js
+++ contact us/contact us.js
@@ -1,24 +1,27 @@
 emailjs.init("nKyezAY8dl7ulg8t1");
-emailjs.sendform("contact_service", "contact_form", this);
-
-let subButton = document.getElementById("submit_button");
-var gmailInput = document.getElementById("gmail_input");
+var gmail = document.getElementById("gmail_input");
 var comments = document.getElementById("comments_input");
+var subButton = document.getElementById("submit_button");
 
-/*-------------------------*/
+subButton.addEventListener("click", function (value) {
+	var templateParams = {
+		name: gmail.value,
+		notes: comments.value,
+	};
 
-var templateParams = {
-	to_name: document.getElementById("gmail_input"),
-	message: document.getElementById("comments_input"),
-};
-
-subButton.addEventListener("click", function () {
-	emailjs.send("service_5udvc6o", "template_zwqib4e", templateParams).then(
-		function (response) {
-			console.log("SUCCESS!", response.status, response.text);
-		},
-		function (error) {
-			console.log("FAILED...", error);
-		}
-	);
+	emailjs
+		.send(
+			"service_5udvc6o", // ServiceID
+			"template_zwqib4e", // TemplateID
+			templateParams,
+			"nKyezAY8dl7ulg8t1" // PublicKey
+		)
+		.then(
+			function (response) {
+				console.log("SUCCESS!", response.status, response.text);
+			},
+			function (error) {
+				console.log("FAILED...", error);
+			}
+		);
 });
